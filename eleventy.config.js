29818bfc05f617d017dcc33f4dec77559c263f08
Bruno <moev.biz@moev.biz>
2025-02-26 11:43:26 +0100

    init

---
+++ eleventy.config.js
@@ -0,0 +1,35 @@
+const yaml = require("js-yaml");
+const markdownIt = require("markdown-it");
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("src/assets/js/*.js");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/alpinejs/dist/cdn.js': './assets/js/alpine.js',
+  })
+  
+
+  eleventyConfig.addDataExtension("yaml", contents => {
+    let c = yaml.load(contents);
+    return c;
+  });
+
+  eleventyConfig.setLibrary("md", markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  }));
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: '_includes',
+    },
+    templateFormats : ["md", "njk"],
+    htmlTemplateEngine : "njk",
+  };
+
+};
